194f67d8b79b9d2541b6d9c1f1b1de58c1134807
Yogesh Ambhore <yogeshambhore45@gmail.com>
2023-03-06 21:14:57 +0530

    feat: add custom user model

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -143,6 +143,8 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'users.User'
+
 
 logger = logging.getLogger(__name__)
 
@@ -185,51 +187,3 @@ try:
 except Exception as e:
     print("Logging configuration failed: ", e)
 
-
-
-# import logging
-# import logging.config
-
-# from django.utils.log import DEFAULT_LOGGING
-
-# logger = logging.getLogger(__name__)
-
-# LOG_LEVEL = "INFO"
-
-# try:
-#     logging.config.dictConfig({
-#         "version": 1,
-#         "disable_existing_loggers": False,
-#         "formatters":{
-#             "console":{
-#                 "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-#             },
-#             "file":{
-#                 "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-#             },
-#             "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-#         },
-#         "handlers":{
-#             "console":{
-#                 "class":"logging.StreamHandler",
-#                 "formatter":"console",
-#             },
-#             "file":{
-#                 "level":"INFO",
-#                 "class":"logging.FileHandler",
-#                 "formatter":"file",
-#                 "filename":"logs/real_estate.log",
-#             },
-#             "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-#         },
-#         "loggers":{
-#             "":{"level": "INFO", "handlers":["console", "file"], "propagate":False},
-#             "apps":{
-#                 "level": "INFO", "handlers":["console"], "propagate":False
-#             },
-#             "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-#         }
-#     })
-#     print("Logging configuration successful!")
-# except Exception as e:
-#     print("Logging configuration failed: ", e)
