5c00b8368f31535ad2d7f9c62578a028c67ce060
Yogesh Ambhore <yogeshambhore45@gmail.com>
2023-03-21 19:14:27 +0530

    chore: setup docker with compose

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -14,9 +14,20 @@ SITE_NAME = 'Real Estate'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
     }
 }
