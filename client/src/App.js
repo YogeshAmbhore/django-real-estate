4c11ecfc09a3b35c0b19caa2cb50d6b035056a83
Yogesh Ambhore <yogeshambhore45@gmail.com>
2023-03-24 21:51:43 +0530

    chore: setup nginx and react with docker

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Real Estate</h1>
+      <p>The real tests starts here...</p>
+    </div>
+  );
+}
+
+export default App;
